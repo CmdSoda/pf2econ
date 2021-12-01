03c130580a3f526e3a8a0d678522d743786935a0
Patrick Lüdke <patrick.luedke@acticom.de>
2021-12-01 21:51:46 +0100

    output example for the client

--- internal/pf2e/client.go
+++ internal/pf2e/client.go
@@ -33,13 +33,23 @@ func (c *Client) Run() {
 	}
 }
 
+var reset = "\u001b[0m"
+var white = "\u001b[37;1m"
+var yellow = "\u001b[33;1m"
+
 func layout(g *gocui.Gui) error {
-	maxX, maxY := g.Size()
-	if v, err := g.SetView("hello", maxX/2-7, maxY/2, maxX/2+7, maxY/2+2); err != nil {
+	//maxX, maxY := g.Size()
+	if v, err := g.SetView("hello", 0, 0, 40, 20); err != nil {
+		v.Title = "Ability Scores"
 		if err != gocui.ErrUnknownView {
 			return err
 		}
-		fmt.Fprintln(v, "Hello world!")
+		fmt.Fprintln(v, white+"Str: "+yellow+"12")
+		fmt.Fprintln(v, white+"Dex: "+yellow+"13")
+		fmt.Fprintln(v, white+"Con: "+yellow+"11")
+		fmt.Fprintln(v, white+"Int: "+yellow+"14")
+		fmt.Fprintln(v, white+"Wis: "+yellow+"15")
+		fmt.Fprintln(v, white+"Cha: "+yellow+"14")
 	}
 	return nil
 }
