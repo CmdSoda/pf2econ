3e2d0f778f2abc74912054f843dfc853c19b05d8
Patrick Lüdke <patrick.luedke@acticom.de>
2021-12-01 18:03:58 +0100

    loading and saving a character

--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -2,10 +2,24 @@ package server
 
 import (
 	"fmt"
+	"github.com/CmdSoda/pf2econ/internal/game"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-func Test1(t *testing.T) {
+func TestPf2Server_SaveCharacter(t *testing.T) {
 	s := Pf2Server{}
-	fmt.Println(s)
+	assert.Nil(t, s.Init("testing/test.ini"))
+	c := game.NewCharacter()
+	err := s.SaveCharacter(c, "testchar2.json")
+	assert.Nil(t, err)
+	fmt.Println(c)
+}
+
+func TestPf2Server_LoadCharacter(t *testing.T) {
+	s := Pf2Server{}
+	assert.Nil(t, s.Init("testing/test.ini"))
+	c, err := s.LoadCharacter("testchar2.json")
+	assert.Nil(t, err)
+	fmt.Println(c)
 }
